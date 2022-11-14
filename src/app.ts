701aefe8700d0854c7a0352277cbf4bc55739787
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-11-14 10:21:17 -0300

    get address input data

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,9 @@
-// Code goes here!
+const form = document.querySelector('form')!
+const addressInput = document.getElementById('address')! as HTMLInputElement
+
+function addAddressHandler(event: Event) {
+    event.preventDefault()
+    const enteredAddress = addressInput.value
+}
+
+form.addEventListener('submit', addAddressHandler)
