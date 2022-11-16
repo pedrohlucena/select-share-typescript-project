1b6b1429afc492ec95dff510e139299d6279464f
Pedro H. Lucena <pedrohlucena413@gmail.com>
2022-11-16 19:26:04 -0300

    my solution to the problem

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,15 @@ import axios from 'axios'
 
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
 
+type Coordinates = {
+    lat: number,
+    lng: number
+}
+
 interface GoogleGeocodingApiResponse {
     results: Array<{
         geometry: {
-            location: {
-                lat: number,
-                lng: number
-            }
+            location: Coordinates
         }
     }>,
     status: "OK"| "ZERO_RESULTS"
@@ -17,20 +19,44 @@ interface GoogleGeocodingApiResponse {
 const form = document.querySelector('form')!
 const addressInput = document.getElementById('address')! as HTMLInputElement
 
-function addAddressHandler(event: Event) {
+function appendGoogleMapsSDKScriptTag() {
+    const googleMapsSDKScriptTag = document.createElement('script');
+    googleMapsSDKScriptTag.setAttribute('async', '');
+    googleMapsSDKScriptTag.setAttribute('src', `https://maps.googleapis.com/maps/api/js?key=${GOOGLE_API_KEY}`)
+    const titleHtmlElement = document.querySelector('title')! as HTMLTitleElement
+    titleHtmlElement.insertAdjacentElement('afterend', googleMapsSDKScriptTag)
+}
+
+async function addAddressHandler(event: Event) {
     event.preventDefault()
     const enteredAddress = addressInput.value
 
-    axios.get<GoogleGeocodingApiResponse>
+    const response = await axios.get<GoogleGeocodingApiResponse>
         (`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${GOOGLE_API_KEY}`)
-            .then(response => {
-                if(response.data.status !== "OK") { throw new Error("Could not fetch location!") }
-                const coordinates = response.data.results[0].geometry.location 
-            })
-            .catch(error => {
-                alert(error.message)
-                console.error(error)
-            })
+    
+    if(response.data.status !== "OK") { throw new Error("Could not fetch location!") }
+    const coordinates = response.data.results[0].geometry.location 
+    initMap(coordinates)
 }
 
-form.addEventListener('submit', addAddressHandler)
+function initMap(coordinates: Coordinates) {
+    let map: google.maps.Map;
+    map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+        center: coordinates,
+        zoom: 20,
+    })
+}
+
+async function main() {
+    try {
+        await appendGoogleMapsSDKScriptTag()
+        form.addEventListener('submit', addAddressHandler)
+    } catch (error) {
+        if (error instanceof Error) {
+            alert(error.message)
+            console.error(error)
+        }
+    }
+}
+
+main()
